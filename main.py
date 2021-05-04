dab906b4103c0520a564d1cb5ba98a86ae3f6b1f
Damian Costales <39567808+JankSoftware@users.noreply.github.com>
2021-05-04 09:40:18 -0600

    Remove video window
    the footprint for this can be smaller if the video is ignored

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ searchTerms = input('What do you want to hear?')
 results = YoutubeSearch(searchTerms, max_results=1).to_dict()
 
 if len(results) > 0:
-    print(f'I found { results[0]["title"] }. Playing...')
+    print(f'I found { results[0]["title"] }. \nPlaying...')
     # returns a dictionary
-    url = results[0]['id']; #"https://www.youtube.com/watch?v=cCq0P509UL4"
+    url = results[0]['id']; #looks like "https://www.youtube.com/watch?v=xxxxxxxxxxx"
     video = pafy.new(url)
     best = video.getbest(preftype="mp4")
     playurl = best.url
 
     Instance = vlc.Instance()
-    player = Instance.media_player_new()
+    player = Instance.media_player_new('--no-video')
     Media = Instance.media_new(playurl)
     Media.get_mrl()
     player.set_media(Media)
