980fc13f4ded56df91423cdacc82b50221e4d9c2
Damian Costales <39567808+JankSoftware@users.noreply.github.com>
2021-05-04 19:56:39 -0600

    Adding more functionality and the beginning of a time bar

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pafy
+import time
 import vlc
 from youtube_search import YoutubeSearch
 
 def pause():
     programPause = input("Press any key to exit...")
 
-searchTerms = input('What do you want to hear?')
+searchTerms = input('What do you want to hear? ')
 
-results = YoutubeSearch(searchTerms, max_results=1).to_dict()
+results = YoutubeSearch(searchTerms, max_results=5).to_dict()
 
 if len(results) > 0:
-    print(f'I found { results[0]["title"] }. \nPlaying...')
+    print("Here are the top 5 matches I found:")
+    for x in range(0, len(results)):
+        print(f'{ x+1 }.{ results[x]["title"] }')
+    selection = 0
+    while(selection not in [1,2,3,4,5]):
+        selection = int(input("\nWhich would you like to play? "))
+
+    print(f'Selected: { results[selection-1]["title"] }. \nSpinning up audio stream...')
     # returns a dictionary
-    url = results[0]['id']; #looks like "https://www.youtube.com/watch?v=xxxxxxxxxxx"
-    video = pafy.new(url)
-    best = video.getbest(preftype="mp4")
+    videoId = results[selection-1]['id']; #the code of the video marked in x here "https://www.youtube.com/watch?v=xxxxxxxxxxx"
+    video = pafy.new(videoId)
+    best = video.getbestaudio(preftype="m4a")
+    if best == None:
+        print("unable to find audio only stream, deafaulting to video")
+        best = video.getbest(preftype="mp4")
     playurl = best.url
 
     Instance = vlc.Instance()
@@ -23,6 +34,11 @@ if len(results) > 0:
     Media.get_mrl()
     player.set_media(Media)
     player.play()
-    pause();
+    time.sleep(2)
+    duration = round(player.get_length() / 1000, 0)
+    while player.get_state() == vlc.State.Playing:
+        currentTime = round(player.get_time() / 1000, 0)
+        print(f"▶️  ({currentTime}/{duration})", end = "\r")
+        time.sleep(1)
 else:
     print("no results")
